5fe6cb63d52c2182b5231e4412afe920bd6831f9
FillingVoid7 <sharmabhuwan2000@gmail.com>
2025-03-09 23:18:47 +0545

    add MongoDB adapter for NextAuth and configure module resolution

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,11 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import client from "@lib/db";
+
 
 const handler = NextAuth({
+  adapter: MongoDBAdapter(client),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
