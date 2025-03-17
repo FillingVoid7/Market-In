8eef31ad29c4b1beb8b8f8726249d8615edb3476
FillingVoid7 <sharmabhuwan2000@gmail.com>
2025-03-17 22:52:13 +0545

    feat: add Basic component with integrated BasicTextEditor

---
+++ app/templates/basic/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import BasicTextEditor from "../text_editors/basicTextEditor";
+
+const Basic = () => {
+    return (
+        <div>
+            <BasicTextEditor
+            />
+        </div>
+    );
+}
+
+export default Basic;
