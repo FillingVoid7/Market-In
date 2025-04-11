71aabf84deff4496a13edb00613b0fa57104bad2
FillingVoid7 <sharmabhuwan2000@gmail.com>
2025-04-11 22:29:43 +0545

    feat:worked on basic previews redux slice

---
+++ redux/templatesPreview/basicPreviewStore.ts
@@ -0,0 +1,12 @@
+// import { configureStore } from "@reduxjs/toolkit";
+// import freePreviewReducer from "./freePreviewSlice";
+
+// const store = configureStore({
+//   reducer: {
+//     freePreview: freePreviewReducer, 
+//   },
+// });
+
+// export default store;
+// export type RootState = ReturnType<typeof store.getState>;
+// export type AppDispatch = typeof store.dispatch;
